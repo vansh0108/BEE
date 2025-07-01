462f5585b30c2167da1a7aaa650e9384eaba568b
vansh0108 <vanshsarna10@gmail.com>
2025-07-01 14:19:37 +0530

    Initial commit

---
+++ Lec1/app.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}   
+function multiply(a, b) {
+  return a * b;
+}
+add(2, 3);
+subtract(5, 2);
+multiply(4, 3);

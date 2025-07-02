87d47411170e0cac2e2c13e380ab4ed2da8b12e6
vansh0108 <vanshsarna10@gmail.com>
2025-07-02 14:53:32 +0530

    new

--- Lec2/index.js
+++ Lec2/index.js
@@ -4,7 +4,9 @@ function sum(a, b) {
 function subtract(a, b) {
   return a - b;
 }
-module.exports = {
-  sum,
-  subtract
-};
+// module.exports = {
+//   sum,
+//   subtract
+// };
+module.exports.add = sum;
+module.exports.sub = subtract;

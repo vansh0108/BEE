fd18025d5edcb4dd5a2e40e65717bf3e8f7ae396
vansh0108 <vanshsarna10@gmail.com>
2025-07-22 15:49:31 +0530

    new

--- Lec9/index.js
+++ Lec9/index.js
@@ -21,12 +21,30 @@ app.post('/users', function(req, res) {
     let name = req.body.name;
     let password = req.body.password; 
     const userData = { name, password };
-    fs.appendFile('user.txt', JSON.stringify(userData) + '\n', (err) => {
-        if (err) {
-            return res.status(500).json({ error: 'Failed to write file' });
+    let allUsers = [];
+    fs.readFile('user.json', 'utf8', function(err, data){
+        if(err) return res.json(err);
+        if (data && data.length > 0) {
+            allUsers = JSON.parse(data);
         }
-        res.json(userData);
+        allUsers.push(userData);
+        fs.writeFile('user.json', JSON.stringify(allUsers), function(err) {
+            if (err) return res.send(err);
+            res.json(userData);
+        });
     });
+
+
+
+
+    // allUsers.push(userData);
+    // console.log(allUsers);
+    // fs.appendFile('user.json', JSON.stringify(allUsers,null,2) + '\n', (err) => {
+    //     if (err) {
+    //         return res.status(500).json({ error: 'Failed to write file' });
+    //     }
+    //     res.json(userData);
+    // });
 });
 
 app.listen(3000, function() {   

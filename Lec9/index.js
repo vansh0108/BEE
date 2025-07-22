c0c0e180e5a7cc2f8a24cdc44a3ad5da01576332
vansh0108 <vanshsarna10@gmail.com>
2025-07-22 15:01:02 +0530

    new

--- Lec9/index.js
+++ Lec9/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 
 app.use(express.json()); 
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
+// app.get('/',(req,res)=>{
+//     res.send("Hello World");
+// })
 
 
 
 //Post request with callback
-app.post('/senddata', function(req, res) {
-    const { name, email } = req.body; 
-    res.json({ name, email });        
-});
+// app.post('/senddata', function(req, res) {
+//     const { name, email } = req.body; 
+//     res.json({ name, email }); 
+//     console.log(req.body);       
+// });
 
+app.post('/users', function(req, res) {
+    let name = req.body.name;
+    let password = req.body.password; 
+    const userData = { name, password };
+    fs.writeFile('user.txt', JSON.stringify(userData), (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to write file' });
+        }
+        res.json(userData);
+    });
+});
 
 app.listen(3000, function() {   
     console.log('server started');

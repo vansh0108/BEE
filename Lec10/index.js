1a79f3219e82e895271de505427564dfabe6de5c
vansh0108 <vanshsarna10@gmail.com>
2025-07-23 15:18:53 +0530

    new

--- Lec10/index.js
+++ Lec10/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 
-
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static('public'));
+app.use(express.json());
+
 app.get('/', (req,res)=>{
    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/about', (req,res)=>{
-   res.sendFile(__dirname + '/about.html');
+
+app.get('/addUser', (req,res)=>{
+   res.sendFile(__dirname + '/addUser.html');
+});
+app.post('/users', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    res.json({
+        username,
+        password
+    });
 });
 
 app.listen(3000,() => {

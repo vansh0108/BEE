dd5c87b5e71fdb63de17eebf4109aca0743a227a
vansh0108 <vanshsarna10@gmail.com>
2025-08-06 15:36:29 +0530

    axios

--- Lec15/axios/index.js
+++ Lec15/axios/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();  
 
+app.post('/blog', async (req, res) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    console.log(title, description);
+    res.json({
+        success: true,
+        message: 'Blog created successfully',
+})
+});
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });

e087c56d2f803f599cac4e3d7e9c771e0247cddb
vansh0108 <vanshsarna10@gmail.com>
2025-08-12 14:22:43 +0530

    lec16

--- Lec16/index.js
+++ Lec16/index.js
@@ -5,7 +5,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const Blogs=require("./model/users")
+const Blogs=require("./model/userSchema")
 const Users=require("./model/userSchema")
 
 app.post("/blogs",async(req,res)=>{

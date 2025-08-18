5f6b57db919aed28d5e22d23c219f87fc2d85157
vansh0108 <vanshsarna10@gmail.com>
2025-08-18 15:28:54 +0530

    lec 19

--- Lec19/routes/userRoutes.js
+++ Lec19/routes/userRoutes.js
@@ -1,40 +1,11 @@
-const express = require("express")
-const router = express.Router()
-const user = require("../model/userSchema")
-// Import the necessary models
-router.post("/", async (req, res) => {
-  let { email, username, password } = req.body;
-  let newUser = new Users({
-    email: email,
-    username: username,
-    password: password
-  });
-  await newUser.save();
-  res.json({
-    success: true,
-    data: newUser,
-    message: "user added successfully!!!"
-  });
-});
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/userController");
 
-router.get("/", async (req, res) => {
-  let allUsers = await Users.find();
-  res.json({
-    success: true,
-    data: allUsers
-  });
-});
+router.post("/", userController.postAddUser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.delete("/:id", userController.deleteUser);
+router.put("/:id", userController.updateUser);
 
-router.get("/:id", async (req, res) => {
-  let { id } = req.params;
-  console.log(id)
-  let userExist = await Users.findOne({ _id: id }).populate("blogs")
-  console.log(userExist);
-  if(userExist){
-    res.json({
-    success: true,
-    data: userExist
-  });
-  }
-});
-module.exports = router
+module.exports = router;

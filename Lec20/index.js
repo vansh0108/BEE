47cc1fb2bf34e8676b0e28933d86fd154ed514e2
vansh0108 <vanshsarna10@gmail.com>
2025-08-19 14:45:31 +0530

    lec 20

--- Lec20/index.js
+++ Lec20/index.js
@@ -1,13 +1,20 @@
 //middleware - function which run on client request before controller functions
 const express = require('express');
+const { m1, m2 } = require('./middleware/firstmiddleware');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname+'public'));
+app.use(express.static(__dirname+'/public'));
+app.use(m1);
+app.use(m2);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the User Management API");
+app.get("/health", (req, res) => {
+  console.log("running controller");
+  res.json({
+    status: "ok",
+    message: "Server is healthy",
+  })
 });
 
 app.listen(3000, () => {

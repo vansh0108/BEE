10626285aeff29b77c25d5b98a90ad05b7b83f76
vansh0108 <vanshsarna10@gmail.com>
2025-08-19 15:35:51 +0530

    lec 20

--- Lec20/index.js
+++ Lec20/index.js
@@ -1,22 +1,28 @@
 //middleware - function which run on client request before controller functions
 const express = require("express");
 const { m1, m2 } = require("./middleware/firstmiddleware");
+const { m3 } = require("./middleware/pathlevel");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(m1);
-app.use(m2);
-app.get("/health", (req, res) => {
+
+app.use("/api/users", userRoutes);
+
+app.get("/health", m3, (req, res, next) => {
    console.log("running controller function");
    res.json({ status: "OK",
     message: "Server is healthy",
     });
   
 });
-app.get("/home", (req, res) => {
+app.use(m2);
+app.get("/home", (req, res, next) => {
     console.log("running controller function");
     res.json({  success: true,
     message: "Welcome to the home page",

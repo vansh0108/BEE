7dde3a938b171e16426facf2dc2a5cef129d555d
vansh0108 <vanshsarna10@gmail.com>
2025-08-19 15:09:40 +0530

    lec 20

--- Lec20/index.js
+++ Lec20/index.js
@@ -1,22 +1,32 @@
 //middleware - function which run on client request before controller functions
-const express = require('express');
-const { m1, m2 } = require('./middleware/firstmiddleware');
+const express = require("express");
+const { m1, m2 } = require("./middleware/firstmiddleware");
+
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname+'/public'));
 app.use(m1);
 app.use(m2);
-
 app.get("/health", (req, res) => {
-  console.log("running controller");
-  res.json({
-    status: "ok",
+   console.log("running controller function");
+   res.json({ status: "OK",
     message: "Server is healthy",
-  })
+    });
+  
+});
+app.get("/home", (req, res) => {
+    console.log("running controller function");
+    res.json({  success: true,
+    message: "Welcome to the home page",
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log(`Server is running on port 3000`);
 });
--- Lec20/middleware/firstmiddleware.js
+++ Lec20/middleware/firstmiddleware.js
@@ -1,14 +1,14 @@
-function m1(req, res) {
-    console.log("Middleware 1 executed");
-    req.userId = "4";
+function m1(req, res, next) {
+    console.log("Middleware 1");
+    req.userId = "4"
+    next(); 
 }
-
-function m2(req, res) {
-    console.log("Middleware 2 executed");
+function m2(req, res, next) {
+    console.log("Middleware 2");
     console.log(req.userId);
     req.isAdmin = true;
+    next(); 
     
 }
-
 module.exports.m1 = m1;
 module.exports.m2 = m2;

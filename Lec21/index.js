e6a761b312198fb9a2329853d431dbe88d42b413
vansh0108 <vanshsarna10@gmail.com>
2025-08-20 14:44:00 +0530

    lec 21

--- Lec21/index.js
+++ Lec21/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const User = require('./model/user');
+app.use(express.json());
+
+
 
 app.get('/health', (req, res) => {
     res.json({
@@ -10,11 +14,35 @@ app.get('/health', (req, res) => {
     });
 });
 
+//endpoint to create a new user in db
+app.post('/api/users/signup', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 
 
 
-mongoose.connect("mongodb://localhost:27017/blogapp")
+
+mongoose.connect("mongodb://localhost:27017/blogapp", {
+    
+})
 .then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {

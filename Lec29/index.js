f2ba2c33dedf9faebfa7ed894e7b27dddd1d1a33
vansh0108 <vanshsarna10@gmail.com>
2025-09-16 15:16:07 +0530

    new

--- Lec29/index.js
+++ Lec29/index.js
@@ -12,5 +12,36 @@ async function addUser(email,name,password){
   return newUser;
 }
 
-addUser("vansh@gmail.com","vansh","12345")
-.then(()=>{console.log("User added");})
+// addUser("vansh@gmail.com","vansh","12345")
+// .then(()=>{console.log("User added");})
+
+async function addTweet(content,userId){
+    let newTweet = await prisma.tweet.create({
+        data:{
+            content:content,
+            userId:userId
+        }
+    })
+    return newTweet;
+
+}
+
+// addTweet("My first tweet",1)
+// .then(()=>{
+//     console.log("Tweet added");
+// })
+
+async function getUserTweet(userId){
+    let tweets = await prisma.tweet.findMany({
+        where:{
+            userId:Number(userId)
+        }
+    })
+    return tweets;
+}
+
+    getUserTweet(1).then(tweets => {
+        console.log(tweets);
+    });
+
+

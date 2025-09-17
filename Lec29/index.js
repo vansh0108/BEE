6cec97e64d07b0792402c17305a2875b53c4e8cb
vansh0108 <vanshsarna10@gmail.com>
2025-09-17 15:06:28 +0530

    new

--- Lec29/index.js
+++ Lec29/index.js
@@ -1,85 +1,107 @@
 const {PrismaClient} = require('./generated/prisma');
 const prisma = new PrismaClient();
-async function addUser(email,name,password){
-   let newUser = await prisma.user.create({
-    data:{
-       email:email,
-       name:name,
-       password:password
-    }
 
-  })
-  return newUser;
-}
+//Add User
+// async function addUser(email,name,password){
+//    let newUser = await prisma.user.create({
+//     data:{
+//        email:email,
+//        name:name,
+//        password:password
+//     }
 
-// addUser("sahil@gmail.com","sahil","12345")
+//   })
+//   return newUser;
+// }
+
+// addUser("yug@gmail.com","yug","12345")
 // .then(()=>{console.log("User added");})
 
-async function addTweet(content,userId){
-    let newTweet = await prisma.tweet.create({
-        data:{
-            content:content,
-            userId:userId
-        }
-    })
-    return newTweet;
+//Add Tweet
+// async function addTweet(content,userId){
+//     let newTweet = await prisma.tweet.create({
+//         data:{
+//             content:content,
+//             userId:userId
+//         }
+//     })
+//     return newTweet;
 
-}
+// }
 
 // addTweet("My first tweet",3)
 // .then(()=>{
 //     console.log("Tweet added");
 // })
 
-async function getUserTweet(userId){
-    let tweets = await prisma.tweet.findMany({
-        where:{
-            userId:Number(userId)
-        }
-    })
-    return tweets;
-}
+//Get User Tweet
+// async function getUserTweet(userId){
+//     let tweets = await prisma.tweet.findMany({
+//         where:{
+//             userId:Number(userId)
+//         }
+//     })
+//     return tweets;
+// }
 
     // getUserTweet(1).then(tweets => {
     //     console.log(tweets);
     // });
 
-async function updateTweet(tweetId,userId,updatedContent){
-   let tweet = await prisma.tweet.findUnique({
-      where: {
-         id: Number(tweetId)
-      }
-   });
-   if(!tweet){
-      return "Tweet not found";
-   }
-   if(tweet.userId != Number(userId)){
-      return "user can not update";
-   }
-   await prisma.tweet.update({
-      where:{
-         id:Number(tweetId)
-      },
-      data:{ 
-         content:updatedContent
-      }
-   });
-}
+//Update Tweet
+// async function updateTweet(tweetId,userId,updatedContent){
+//    let tweet = await prisma.tweet.findUnique({
+//       where: {
+//          id: Number(tweetId)
+//       }
+//    });
+//    if(!tweet){
+//       return "Tweet not found";
+//    }
+//    if(tweet.userId != Number(userId)){
+//       return "user can not update";
+//    }
+//    await prisma.tweet.update({
+//       where:{
+//          id:Number(tweetId)
+//       },
+//       data:{ 
+//          content:updatedContent
+//       }
+//    });
+// }
 // updateTweet("1","1","Updated tweet content")
 // .then(()=>{
 //    console.log("Tweet updated");
 // });
 
 
-async function deleteUser(userId){
-   await prisma.user.delete({
-      where:{
-         id:Number(userId)
-      }
-   });
-}
+//Delete User
+// async function deleteUser(userId){
+//    await prisma.user.delete({
+//       where:{
+//          id:Number(userId)
+//       }
+//    });
+// }
 
-deleteUser(3)
-.then(()=>{
-   console.log("User deleted");
-})
+// deleteUser(1)
+// .then(()=>{
+//    console.log("User deleted");
+// })
+
+
+
+   async function printAllUserEmailName() {
+      const users = await prisma.user.findMany({
+         select: {
+            email: true,
+            name: true
+         }
+      });
+      users.forEach(user => {
+         console.log(`Email: ${user.email}, Name: ${user.name}`);
+      });
+   }
+
+   printAllUserEmailName();

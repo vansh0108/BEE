b2e2894215fed3dc38035d70e3494e5141efba69
vansh0108 <vanshsarna10@gmail.com>
2025-10-07 14:41:45 +0530

    lec33

---
+++ Lec33/backend/server.js
@@ -0,0 +1,50 @@
+const {WebSocketServer} = require("./node_modules/ws");
+const wss = new WebSocketServer({port:8888});
+
+// wss.on("connection", function(socket){
+//     console.log("User connected");
+//     socket.on("message", function(msg){
+//         console.log("Message received: " + msg);
+//         if(msg.toString() === "ping"){
+//             socket.send("pong");
+//         }
+        
+//     });
+// })
+
+// let allSocket = [];
+// wss.on("connection", function(socket){
+//     console.log("User connected");
+//     allSocket.push(socket);
+//     socket.on("message", function(msg){
+//         console.log("Message received: " + msg.toString());
+//         if(msg.toString() === "ping"){
+//             socket.send("pong");
+//         }
+//         allSocket.forEach((s)=>{
+//             s.send(msg.toString());
+//         }) 
+//     });
+// })
+
+
+let rooms = new Map();
+
+
+wss.on("connection", (socket)=>{
+    console.log("User connected");
+    socket.on("message", function(message){
+          let parsedMessage = JSON.parse(message);
+          let {type, payload} = parsedMessage;
+            if(type == "join"){
+                let {roomId} = payload;
+                if(!rooms.get(roomId)){
+                    rooms.set(roomId, new Set());
+                }
+                rooms.get(roomId).add(socket);
+                console.log(rooms);
+                socket.send("added to room");
+
+            }
+    });
+})

583d68a28211f477d44c275f35bc7112cc338afb
vansh0108 <vanshsarna10@gmail.com>
2025-10-29 14:41:06 +0530

    new

--- stockexchange/order.js
+++ stockexchange/order.js
@@ -28,6 +28,61 @@ class OrderBook{
 
         }
     }
+    //   explain the logic why bids and ask are sorted in such way ?
+    // Ans - Bids are sorted in descending order because buyers want to pay the lowest price possible, so higher bids take precedence.
+    // Asks are sorted in ascending order because sellers want to sell at the highest price possible, so lower asks take precedence.
+    
+
+    //function to place new order in orderbook
+    /* 
+    1. create new order {orderId,side,type,price?,orignqty,remainingqty,execqty,timestamp,user}
+    2. match type if type == market , call marketMatch else call limitMatch
+    
+    */
+
+    placeholder(symbol,side,type,price=null,quantity,user){
+        /* Baasic Validation */
+        let order = {
+            orderId : this._genOrderId(),
+            symbol : this.symbol,
+            side : side,
+            type : type,
+            price : price,
+            orignqty : quantity,
+            remainingqty : quantity,
+            execqty : 0,
+            timestamp : Date.now(),
+            user : user
+        }
+        if(type==="MARKET"){
+            this._marketMatch(order);
+        }else{
+            this._limitMatch(order);
+        }
+    }
+    //execute order if it is market order
+    /* 
+    bids: [] sorted descending
+    ask: [] sorted ascending
+    1. type: buy/sell
+    2. if buy start buying from asks array starting from index 0.
+    loop while order remainingqty >0 and asks.length>0
+    buy min(order.remainingqty,asks[0].remainingqty)
+    update remainingqty and execqty of both orders
+  
+
+    */
+    _marketMatch(order){
+       if(order.side==="BUY"){
+
+
+       }
+
+    }
+    //execute order if it is limit order
+    _limitMatch(){
+        
+    }
 }
 //if a function or variable start with (private)
 //let orderbook = new OrderBook("BTCUSD")
